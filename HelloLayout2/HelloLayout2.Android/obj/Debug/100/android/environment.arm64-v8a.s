a1f6853299d99b40ed25a9feb12eebdedaacd8dd
Karen <karenten10@gmail.com>
2020-12-18 17:20:09 +0800

    android: change app name

--- HelloLayout2/HelloLayout2.Android/obj/Debug/100/android/environment.arm64-v8a.s
+++ HelloLayout2/HelloLayout2.Android/obj/Debug/100/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"416591d3-f035-4a2e-8018-9b23254101fc"
+	.asciz	"1920047c-ffd7-4618-8621-ea5c3be3d823"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
