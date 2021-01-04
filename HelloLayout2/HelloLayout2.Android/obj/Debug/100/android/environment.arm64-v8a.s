bdbd8fb3a2b18bb368e5b28a83cb815d3a16557f
Karen <karenten10@gmail.com>
2021-01-04 15:51:46 +0800

    add default image

--- HelloLayout2/HelloLayout2.Android/obj/Debug/100/android/environment.arm64-v8a.s
+++ HelloLayout2/HelloLayout2.Android/obj/Debug/100/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"f81f62f6-016c-40e7-bd11-1e772880fa9f"
+	.asciz	"be6eab12-6fb4-485d-ab49-57004ce6b6ac"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
